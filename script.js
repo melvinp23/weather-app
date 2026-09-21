028e7f1f7b6ce583cf86676c1ec6005be78f6108
melvin <melvin.pamplona@gmail.com>
2026-09-21 19:57:09 +0800

    Add Button to  weather data on London and test to  append address to the <p> element

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+const getWeatherBtn = document.getElementById('get-weather');
+const addressDiv = document.querySelector('p');
 const api_key = 'BKULCM6WCG2SXVP8Y4EU7SY3W';
 const print =
 	'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=';
+
+console.log(addressDiv.textContent);
 
 async function getWeather() {
 	try {
@@ -8,7 +12,11 @@ async function getWeather() {
 			`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=${api_key}`,
 		);
 		const weatherData = await response.json();
-		console.log(weatherData);
+		addressDiv.textContent = weatherData.address;
+		document.body.appendChild(addressDiv);
+		getWeatherBtn.addEventListener('click', () => {
+			console.log(weatherData);
+		});
 	} catch (error) {
 		console.error(error);
 	}
